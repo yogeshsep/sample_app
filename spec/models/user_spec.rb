47266344704d1d8a631e20c888071fba5d14870f
yogeshsep <yogeshsep15@gmail.com>
2014-01-07 10:56:10 +0530

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ describe User do
 
  before do
     @user = User.new(name: "Example User", email: "user@example.com",
-    password: "foobar", password_confirmation: "foobar")
- end
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
@@ -19,19 +19,25 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
-
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
   it { should be_valid }
   it { should_not be_admin }
 
-   describe "with admin attribute set to 'true'" do
-     before do
+  describe "with admin attribute set to 'true'" do
+    before do
       @user.save!
       @user.toggle!(:admin)
-     end
+    end
 
     it { should be_admin }
-   end
-  
+  end
+
+  it { should be_valid }
 
   describe "when name is not present" do
       before { @user.name = " " }
@@ -113,18 +119,20 @@ describe "when email format is invalid" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
-
   describe "micropost associations" do
 
     before { @user.save }
-    let!(:older_micropost) do 
+    let!(:older_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
     let!(:newer_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
 
-    it "should destroy associated microposts" do
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+     it "should destroy associated microposts" do
       microposts = @user.microposts.dup
       @user.destroy
       microposts.should_not be_empty
@@ -132,19 +140,51 @@ describe "when email format is invalid" do
         Micropost.find_by_id(micropost.id).should be_nil
       end
     end
-    
-    describe "status" do
+   
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
+
+describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
 
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
-    end
-    
-    it "should have the right microposts in the right order" do
-      @user.microposts.should == [newer_micropost, older_micropost]
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 end
